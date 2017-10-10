cc6a44f7f39a7a8c4769c7725e7db75716167ae7
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-10 14:08:28 -0500

    completed

--- client.js
+++ client.js
@@ -1,20 +1,35 @@
 $(document).ready(readyNow);
-
+var count = 0;
+var colorCount = 0;
 function readyNow() {
   console.log('ready!');
   $('#generateButton').on('click', newDiv);
-  $('#swap').on('click', swapColor);
+  $('#firstDiv').on('click', '#swap', swapColor);
+  $('#firstDiv').on('click', '#delete', deleteDiv);
 }
-
 function newDiv() {
+  count++;
   console.log('newDiv');
-
-  $('#firstDiv').append('<div>new div</div>');
-  $('#firstDiv').children().append('<p>counter</p>');
-  $('#firstDiv').last().append('<button type="button" id="swap">swap</button>' +
-  '<button type="button" id="delete">delete</button>');
+  if (count == 1) {
+  $('#firstDiv').append('<div></div>');
+  $('#firstDiv').children().append('<p id="count">' + count + '</p>');
+  $('#firstDiv').last().append('<button type="button" id="swap">swap</button>');
+  $('#firstDiv').last().append('<button type="button" id="delete">delete</button>');
 }
-
+  else {
+    $('#count').html('<p id="count">' + count + '</p>');
+  }
+}
   function swapColor() {
-    $('#firstDiv').parent().css('background-color', 'yellow');
+    colorCount++;
+    console.log('colors?');
+    if (colorCount % 2) {
+    $('#firstDiv').css('background-color', 'yellow');
+  }
+    else {
+    $('#firstDiv').css('background-color', 'red');
+    }
+  }
+  function deleteDiv() {
+    $('#firstDiv').hide();
   }
